492ceba919d6c74dc7630d884eb16496f7870083
derekmeegan <derek.j.meegan@gmail.com>
2024-08-14 18:57:37 -0400

    testing this out

--- ui.py
+++ ui.py
@@ -16,6 +16,7 @@ from typing import List
 
 st.set_page_config(page_title = 'AmerikickGPT')
 hide_github_icon = """<style>
+.css-1jc7ptx, .e1ewe7hr3, .viewerBadge_container__1QSob, .styles_viewerBadge__1yB5_, .viewerBadge_link__1S137, .viewerBadge_text__1JaDK{ display: none; }
 #MainMenu {
   visibility: hidden;
 }
